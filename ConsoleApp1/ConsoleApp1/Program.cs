414b3c79f1a76ff3fe71d23c8883c051a09d43dd
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-05-15 11:01:39 +1200

    classes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Al = new Animal("", "");
+            Console.WriteLine("*********** task 1 **************");
+            Console.WriteLine("please enter the animals name");
+            Al.Name = Console.ReadLine();
+            Console.WriteLine("please enter the animal sound");
+            Al.Sound = Console.ReadLine();
+
+            Console.WriteLine("\n" + A1.AnimalSays());
+
+            Console.ReadLine();
+
+            Console.WriteLine("*********** task 1 ************");
+            Console.WriteLine("please enter the animal name");
+            string name = Console.ReadLine();
+            Console.WriteLine("please enter the aniumal sound");
+            string sound = Console.ReadLine();
+            Animal A2 = new Animal(name, sound);
+            Console.WriteLine("\n" + A2.AnimalSays());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Animal
+    {
+        string name;
+        string sound;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+        public string Sound
+        {
+            get { return sound; }
+            set { sound = value; }
+        }
+         public string AnimalSays()
+        {
+            return $"The{Name} says {Sound}";
+        }
+        public Animal( string _name, string _sound)
+        {
+            Name = _name;
+            Sound = _sound;
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class program
+    {
+        static void Main(string[] args)
+        { 
+        Console.WriteLine("*************** task 2 **********");
+        Console.WriteLine("Enter your person details below...");
+        Console.WriteLine("First name");
+        string fname = Console.ReadLine();
+        Console.WriteLine("Last name");
+        string lame = Console.ReadLine();
+            Console.WriteLine("Tax rate: ");
+            d
+
+
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Gross { get; set; }
+        public double Tax { get; set; } = 0.15;
+
+        //method
+        public double NetSalary()
+        {
+            return Gross * (1 - Tax);
+        }
+        //constructer
+        public Employee(string fname, string lname, double money)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Gross = money;
+        }
+        public Employee()
+        }
+    }
+}
